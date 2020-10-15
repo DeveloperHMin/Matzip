1f7b9f3d6bc9c687b7a79d2e964e37107f5d7fdf
hw856 <hw856@DESKTOP-6FHFP44>
2020-10-15 12:05:13 +0900

    막내야 해보거라

---
+++ MatZipSpring/src/main/java/com/koreait/matzip/user/UserMapper.java
@@ -0,0 +1,23 @@
+package com.koreait.matzip.user;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.koreait.matzip.user.model.UserDMI;
+import com.koreait.matzip.user.model.UserPARAM;
+import com.koreait.matzip.user.model.UserVO;
+
+@Mapper
+public interface UserMapper {
+	int insUser(UserVO p);
+	int insFavorite(UserPARAM param);
+	
+	UserDMI selUser(UserPARAM p);
+	
+	int delFavorite(UserPARAM param);
+}
+
+
+
+
+
+
